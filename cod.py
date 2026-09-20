4612d03d813931b7c95e75103191899977e452b0
Henry Soares <Henrysoares0723@gmail.com>
2026-09-20 15:45:11 -0300

    Tratando/compilando  base de dados

--- cod.py
+++ cod.py
@@ -3,12 +3,15 @@ import pandas as pd
 
 lista_arquivos = os.listdir("Curso Básico de Python/Curso Básico de Python/Vendas")
 print(lista_arquivos)
+
+tabela_total = pd.DataFrame()
+
 for arquivo in lista_arquivos:
     print(arquivo)
     print(f'Curso Básico de Python/Curso Básico de Python/Vendas/{arquivo}')
     if 'Vendas' in arquivo:
-        print(f'Curso Básico de Python/Curso Básico de Python/Vendas/{arquivo}')
-    else:
-        print('Arquivo de devolução')
+        tabela = pd.read_csv(f'Curso Básico de Python/Curso Básico de Python/Vendas/{arquivo}')
+        tabela_total = tabela_total.append(tabela)
 
+print(tabela_total)
 
