bcb167497649ca7755d21da67bc7041ee54983a9
Henry Soares <Henrysoares0723@gmail.com>
2026-09-21 19:45:01 -0300

    Adicionando grafico/dashboard de faturamento

--- cod.py
+++ cod.py
@@ -1,19 +1,36 @@
 import os
-import pandas as pd 
-
+import pandas as pd
+import plotly.express as px
 
 lista_arquivos = os.listdir("Curso Básico de Python/Curso Básico de Python/Vendas")
 
 tabela_total = pd.DataFrame()
-for arquivo in lista_arquivos:
-    if 'Vendas' in arquivo:
-        tabela = pd.read_csv(f'Curso Básico de Python/Curso Básico de Python/Vendas/{arquivo}')
-        tabela_total = pd.concat([tabela_total, tabela])
- 
-tabela_total = tabela_total.groupby('Produto').sum()
-tabela_total = tabela_total[['Quantidade Vendida' , 'Preco Unitario']].sort_values(by='Quantidade Vendida' , ascending=False)
-tabela_total['Faturamento'] = tabela_total['Quantidade Vendida'] * tabela_total['Preco Unitario']
-tabela_faturamento = tabela_total.groupby('Produto').sum()
-tabela_faturamento = tabela_faturamento[['Faturamento']].sort_values(by='Faturamento' , ascending=False)
-print(tabela_total)
 
+for arquivo in lista_arquivos:
+    if "Vendas" in arquivo:
+        tabela = pd.read_csv(f"Curso Básico de Python/Curso Básico de Python/Vendas/{arquivo}")
+        tabela_total = pd.concat([tabela_total, tabela])
+
+tabela_produtos = tabela_total.groupby("Produto").sum()
+tabela_produtos = tabela_produtos[["Quantidade Vendida"]].sort_values(
+    by="Quantidade Vendida", ascending=False
+)
+
+print(tabela_produtos)
+
+tabela_total["Faturamento"] = tabela_total["Quantidade Vendida"] * tabela_total["Preco Unitario"]
+
+tabela_faturamento = tabela_total.groupby("Produto").sum()
+tabela_faturamento = tabela_faturamento[["Faturamento"]].sort_values(
+    by="Faturamento", ascending=False
+)
+
+print(tabela_faturamento)
+
+tabela_lojas = tabela_total.groupby("Loja").sum()
+tabela_lojas = tabela_lojas[["Faturamento"]]
+
+print(tabela_lojas)
+
+grafico = px.bar(tabela_lojas, x=tabela_lojas.index, y="Faturamento")
+grafico.show()
